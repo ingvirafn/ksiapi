1ed91d860088a419c0bfd428dad84298cc86fb42
ingvirafn <ingvirafn@ingvirafn.com>
2015-06-11 19:23:17 +0000

    readme update

--- app.js
+++ app.js
@@ -13,6 +13,26 @@ var SERVERPORT = process.env.PORT || 3000;
 var cors = require('./lib/cors.js');
 app.use(cors());
 
+// Enable SignatureHeaders
+var expireFilterPassword = 'krapp';
+
+var sh = require('./lib/signatureHeaders.js');
+
+app.get('/getToken', function (req, res) {
+	var expireDate = new Date();
+	expireDate.setDate(expireDate.getDate()+1);
+	var token = sh.generateExpireToken(expireDate, expireFilterPassword);
+	res.json({ Expires: expireDate, Token: token });
+});
+
+app.get('/getToken2', function (req, res) {
+	var expireDate = new Date();
+	expireDate.setDate(expireDate.getDate()-1);
+	var token = sh.generateExpireToken(expireDate, expireFilterPassword);
+	res.json({ Expires: expireDate, Token: token });
+});
+app.use(sh.expireFilter(expireFilterPassword));
+
 // Error handling..
 var onError = function(res, text) {
 	res.json({error: text });
